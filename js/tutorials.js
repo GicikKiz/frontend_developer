a33e1e6bd1d201718a0c4665a0b7554c98ab9a22
GicikKiz <deryagul567@gmail.com>
2023-11-12 00:40:28 +0300

    yeni index ve js oluşturuldu

---
+++ js/tutorials.js
@@ -0,0 +1,10 @@
+console.log("loglama"); // yazılımcının görmesini istiyorsak console log kullanıcaz
+console.info("info");
+console.warn("warn"); // warn uyarı anlamında
+console.error("error"); // error ise yapmış olduğumuz hataları bize console üzerinden gösterir
+
+alert("pop-up bilgilendirme"); // alert ise bize bilgilendirme mesajı gösterir
+
+document.writeln("ekranda yazı");  // ekranda yazı kullanmak istiyorsam document kullanıcaz
+
+// console üzerinden javascript yazılabilir orada yazdıklarımız clear diyerek de silebiliriz. yada alert verebiliriz
