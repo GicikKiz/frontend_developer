21cdac25a6e0ddb9591cae420f808fb950e0773a
GicikKiz <deryagul567@gmail.com>
2023-11-12 01:19:11 +0300

    js ile ilgili notlar

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,10 +1,34 @@
-console.log("loglama"); // yazılımcının görmesini istiyorsak console log kullanıcaz
-console.info("info");
-console.warn("warn"); // warn uyarı anlamında
-console.error("error"); // error ise yapmış olduğumuz hataları bize console üzerinden gösterir
+// console.log("loglama"); // yazılımcının görmesini istiyorsak console log kullanıcaz
+// console.info("info");
+// console.warn("warn"); // warn uyarı anlamında
+// console.error("error"); // error ise yapmış olduğumuz hataları bize console üzerinden gösterir
 
-alert("pop-up bilgilendirme"); // alert ise bize bilgilendirme mesajı gösterir
+// alert("pop-up bilgilendirme"); // alert ise bize bilgilendirme mesajı gösterir
 
-document.writeln("ekranda yazı");  // ekranda yazı kullanmak istiyorsam document kullanıcaz
+// document.writeln("");  // ekranda yazı kullanmak istiyorsam document kullanıcaz
+
+// window.document.writeln(""); 
+// window temel yapı her şeyin en tepesindeki olan yapıdır.window kullansak da  kullanmasak da kodlarda bir şey değişmiyor
 
 // console üzerinden javascript yazılabilir orada yazdıklarımız clear diyerek de silebiliriz. yada alert verebiliriz
+
+// variable de kısaltılması "var" olarak kullanılır.
+
+// console.log(typeof result); yazıldığında ne yazarsak onun ne anlama geldiğini bize söyler. eğer aşağıdaki bilgileri yazarsak parantezin içine typeof yazarsak orada number kullandığımızı bize console gösterir.
+// var result =44.23;
+// console.log(result); 
+
+var result =44.23;
+console.log(result);
+
+var result =4423;
+console.log(typeof result+" => "+result);
+
+var result=true;
+console.log(typeof result+" => "+result);
+
+var result="js öğreniyorum";
+console.log(typeof result+" => "+result);
+
+
+
