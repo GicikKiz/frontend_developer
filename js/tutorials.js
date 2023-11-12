8816240be0535838050e34751e4d415ed1deef0f
GicikKiz <deryagul567@gmail.com>
2023-11-12 23:49:15 +0300

    yorum satırları eklendi

--- js/tutorials.js
+++ js/tutorials.js
@@ -12,7 +12,28 @@
 
 // console üzerinden javascript yazılabilir orada yazdıklarımız clear diyerek de silebiliriz. yada alert verebiliriz
 
+// noktalı virgül bir komutun bittiğini ve başka bir komutun başlamasını belirtir(;)
+
+// blokların başlangıç ve bitiş yerini gösteren şekle küme parantezi denir.({}) süslü parantez de denilebilir.
+
+// javascript dilinde boşluk karakterlerinin bir önemi yoktur. istediğimiz kadar boşluk bırakabiliriz.
+
 // variable de kısaltılması "var" olarak kullanılır.
+
+// sözel veri, degişkenlere atanan karakter veya metinler değişkenin veri tipini string yapar.sözel veri atamaları çift tırnak "" içine atanır.
+var ad="Derya";
+
+// mantıksal veri, eğer bir değişkene true veya false değerini atarsanız bu değişken boolean tipine dönüşür.
+var result=false;
+
+// diziler birden fazla değer taşıyan değişkenlerdir.
+var dersler=["fen", "din", "beden", "matematik"];
+
+// çif tırnak içindeki değişkene tek tırnak kullanılabilir.
+var mekan="derya'nın yeri";
+
+// değişkene değer atanmadıysa değişken değeri undefined olarak görülür.
+document.write(d);
 
 // console.log(typeof result); yazıldığında ne yazarsak onun ne anlama geldiğini bize söyler. eğer aşağıdaki bilgileri yazarsak parantezin içine typeof yazarsak orada number kullandığımızı bize console gösterir.
 // var result =44.23;
